344370238b14be49843d78f1a9b2be2ffeca0d47
kmetarianti-afk <kmetarianti@gmail.com>
2026-02-12 11:27:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 
@@ -5,11 +6,31 @@ app.use(express.json());
 
 const jadwal = {
   senin: [
-    { waktu: "08:00-09:00", mapel: "Matematika" },
-    { waktu: "09:00-10:00", mapel: "Bahasa Indonesia" }
+    { waktu: "08:10-10:10", mapel: "agama  hindu" },
+    { waktu: "10:40-15:00", mapel: "dasar-dasar akuntansi" },
   ],
   selasa: [
-    { waktu: "08:00-09:00", mapel: "IPA" }
+    { waktu: ":07:30-09:30", mapel: "pjok" },
+    { waktu: "09:30-11:20", mapel: "matematika" },
+    { waktu: "11:20-15:40", mapel: "ipas" }
+  ],
+  rabu: [
+    { waktu: "07:30-08:50", mapel: "Matematika" },
+    { waktu: "08:50-13:20", mapel: "dasar-dasar akuntansi" },
+    { waktu: "13:40-15:00", mapel: "kka" }
+  ],
+  kamis: [
+    { waktu: "07:30-08:50", mapel: "bahasa indonesia" },
+    { waktu: "08:50-12:00", mapel: "informatika" },
+    { waktu: "12:00-15:00", mapel: "bahasa inggris" },
+     { waktu: "15:00-15:40", mapel: "bk" }
+  ],
+  jumat: [
+    { waktu: "08:10-09:30", mapel: "seni budaya" },
+    { waktu: "09:30-11:20", mapel: "PKN" },
+    { waktu: "11:20-12:40", mapel: "bahasa indonesia" },
+    { waktu: "12:40-14:20", mapel: "bahasa bali" },
+    { waktu: "14:20-15:40", mapel: "matematika" },
   ]
 };
 
@@ -26,10 +47,10 @@ app.post("/webhook", (req, res) => {
     });
   } else {
     res.json({
-      fulfillmentText: "Jadwal tidak ditemukan"
+      fulfillmentText: "Maaf, jadwal hanya tersedia Senin sampai Jumat."
     });
   }
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Server jalan"));
+app.listen(port, () => console.log("Webhook aktif"));
